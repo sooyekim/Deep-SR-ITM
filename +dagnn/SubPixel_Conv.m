a2bebc34b05814f591b218855a097ae7364fb6fc
Soo Ye Kim <sooyekim@kaist.ac.kr>
2019-09-19 15:13:42 +0900

    Update SubPixel_Conv.m

--- +dagnn/SubPixel_Conv.m
+++ +dagnn/SubPixel_Conv.m
@@ -10,36 +10,36 @@ classdef SubPixel_Conv < dagnn.ElementWise
 % Using an Efficient Sub-Pixel Convolutional Neural Network", CVPR, 2016.
 
   properties
-    scale=2;
+    scale = 2;
   end
 
   methods
     function outputs = forward(obj, inputs, params)
-        input=inputs{1};
-        output=gpuArray(zeros(size(input,1)*obj.scale,size(input,2)*obj.scale,size(input,3)/obj.scale/obj.scale,size(input,4)));
-        for channel = 1:size(input,3)
-            ch=floor((channel-1)/obj.scale/obj.scale)+1;
-            c=mod(channel,obj.scale*obj.scale);
-            if c==0, c=obj.scale*obj.scale; end
-            q=floor((c-1)/obj.scale)+1;
-            r=mod(c, obj.scale);
-            if r==0, r=obj.scale; end          
-            output(q:obj.scale:end,r:obj.scale:end,ch,:)=input(:,:,channel,:);
+        input = inputs{1};
+        output=gpuArray(zeros(size(input, 1)*obj.scale, size(input, 2)*obj.scale, size(input, 3)/obj.scale/obj.scale, size(input, 4)));
+        for channel = 1:size(input, 3)
+            ch = floor((channel-1)/obj.scale/obj.scale)+1;
+            c = mod(channel,obj.scale*obj.scale);
+            if c == 0, c = obj.scale*obj.scale; end
+            q = floor((c-1)/obj.scale)+1;
+            r = mod(c, obj.scale);
+            if r == 0, r = obj.scale; end          
+            output(q:obj.scale:end, r:obj.scale:end, ch, :) = input(:, :, channel, :);
         end
-        outputs{1}=gpuArray(single(output));
+        outputs{1} = gpuArray(single(output));
     end
 
     function [derInputs, derParams] = backward(obj, inputs, params, derOutputs)
-        output=derOutputs{1};
-        input=gpuArray(zeros(size(output,1)/obj.scale,size(output,2)/obj.scale,size(output,3)*obj.scale*obj.scale,size(output,4)));
-        for channel = 1:size(input,3)
-            ch=floor((channel-1)/obj.scale/obj.scale)+1;
-            c=mod(channel,obj.scale*obj.scale);
-            if c==0, c=obj.scale*obj.scale; end
-            q=floor((c-1)/obj.scale)+1;
-            r=mod(c, obj.scale);
-            if r==0, r=obj.scale; end            
-            input(:,:,channel,:)=output(q:obj.scale:end,r:obj.scale:end,ch,:);
+        output = derOutputs{1};
+        input = gpuArray(zeros(size(output, 1)/obj.scale, size(output, 2)/obj.scale, size(output, 3)*obj.scale*obj.scale, size(output, 4)));
+        for channel = 1:size(input, 3)
+            ch = floor((channel-1)/obj.scale/obj.scale)+1;
+            c = mod(channel,obj.scale*obj.scale);
+            if c == 0, c = obj.scale*obj.scale; end
+            q = floor((c-1)/obj.scale)+1;
+            r = mod(c, obj.scale);
+            if r == 0, r = obj.scale; end            
+            input(:, :, channel, :) = output(q:obj.scale:end, r:obj.scale:end, ch, :);
         end
       derInputs{1} = gpuArray(single(input));
       derParams = {} ;
@@ -47,7 +47,7 @@ classdef SubPixel_Conv < dagnn.ElementWise
 
     function obj = SubPixel_Conv(varargin)
       obj.load(varargin) ;
-      obj.scale=obj.scale;
+      obj.scale = obj.scale;
     end
   end
 end
