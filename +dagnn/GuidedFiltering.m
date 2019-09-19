d3ce247c4241ed0a94fe05e2a5d0b54f3c7b6b5f
Soo Ye Kim <sooyekim@kaist.ac.kr>
2019-09-19 15:11:08 +0900

    Update GuidedFiltering.m

--- +dagnn/GuidedFiltering.m
+++ +dagnn/GuidedFiltering.m
@@ -8,39 +8,39 @@ classdef GuidedFiltering < dagnn.ElementWise
 % [1] Kaiming He et al. "Guided Image Filtering", ECCV, 2010.
 
   properties
-    r=5
-    eps=0.01
+    r = 5
+    eps = 0.01
   end
 
   methods
     function outputs = forward(obj, inputs, params)
-        input=inputs{1};
+        input = inputs{1};
         
-        H=(1/(obj.r*obj.r))*ones(obj.r,obj.r);
+        H = (1/(obj.r*obj.r))*ones(obj.r, obj.r);
 
-        meanI=imfilter(input,H,'replicate','same');
+        meanI = imfilter(input, H, 'replicate','same');
 
-        var=imfilter(input.*input,H,'replicate','same')-meanI.*meanI;
-        a=var./(var+obj.eps);
-        b=meanI-a.*meanI;
+        var = imfilter(input.*input, H, 'replicate', 'same')-meanI.*meanI;
+        a = var./(var+obj.eps);
+        b = meanI-a.*meanI;
 
-        meana=imfilter(a,H,'replicate','same');
-        meanb=imfilter(b,H,'replicate','same');
+        meana = imfilter(a, H, 'replicate', 'same');
+        meanb = imfilter(b, H, 'replicate', 'same');
 
-        outputs{1}=meana.*input+meanb;
+        outputs{1} = meana.*input+meanb;
     end
 
     function [derInputs, derParams] = backward(obj, inputs, params, derOutputs)
         % No back propagation needed for Deep SR-ITM
-        derInputs{1}=0*derOutputs{1};
+        derInputs{1} = 0*derOutputs{1};
         derParams = {};
     end
         
 
     function obj = GuidedFiltering(varargin)
-      obj.load(varargin) ;
-      obj.r=obj.r;
-      obj.eps=obj.eps;
+      obj.load(varargin);
+      obj.r = obj.r;
+      obj.eps = obj.eps;
     end
   end
 end
